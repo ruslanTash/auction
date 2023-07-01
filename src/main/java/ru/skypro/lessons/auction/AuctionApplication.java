b57031e7c8b43b0becc430c4243aabec65af712f
ruslanTash <ruslan--t@yandex.ru>
2023-07-02 01:04:50 +0400

    1

---
+++ src/main/java/ru/skypro/lessons/auction/AuctionApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skypro/lessons/auction/AuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
